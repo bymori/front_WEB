bf22f936abc159461c00e4bfeebd05841624e342
by_mori <3172699319@qq.com>
2021-09-13 23:17:32 +0800

    js基础 & 数据类型

---
+++ JavaScript基础&实战/01.JavaScript/js/02-js编写位置.js
@@ -0,0 +1,10 @@
+/*
+ * @Descripttion:
+ * @version:
+ * @Author: by_mori
+ * @Date: 2021-09-13 16:40:57
+ * @LastEditors: by_mori
+ * @LastEditTime: 2021-09-13 17:40:51
+ */
+alert('这里是 外部js 中的代码');
+console.log('这里是 外部js 中的console.log代码');
