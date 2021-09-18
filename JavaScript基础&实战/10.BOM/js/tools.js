7543410a64a031a85adf3a2d8f472eeaf2725e32
by_mori <3172699319@qq.com>
2021-09-19 00:09:10 +0800

    二级菜单

--- JavaScript基础&实战/10.BOM/js/tools.js
+++ JavaScript基础&实战/10.BOM/js/tools.js
@@ -4,7 +4,7 @@
  * @Author: by_mori
  * @Date: 2021-09-18 19:05:09
  * @LastEditors: by_mori
- * @LastEditTime: 2021-09-18 19:06:13
+ * @LastEditTime: 2021-09-18 23:38:51
  */
 /*
  *  定义一个函数，用来获取指定元素的当前的样式
@@ -76,3 +76,53 @@ function move(obj, attr, target, speed, callback) {
     }
   }, 20);
 }
+
+
+      //定义一个函数，，用来向一个元素中添加指定的class属性值
+      /*
+       *  参数：
+       *    obj 要添加class属性的元素
+       *    cn 要添加的class值
+       */
+      function addClass(obj, cn) {
+        if (!hasClass(obj, cn)) {
+          obj.className += ' ' + cn;
+        }
+      }
+
+      /*
+       *  判断一个元素中是否含有指定的class属性值
+       *    如果有该class，则返回true,没有则返回false
+       */
+      function hasClass(obj, cn) {
+        //  判断obj中有没有cn class
+        //  创建一个正则表达式
+        // var reg = /\bcn\b/;
+        var reg = new RegExp('\\b' + cn + '\\b');
+        return reg.test(obj.className);
+      }
+
+      //删除一个元素中的指定的class属性
+      function removeClass(obj, cn) {
+        //  创建一个正则表达式
+        var reg = new RegExp('\\b' + cn + '\\b');
+        // 删除class
+        obj.className = obj.className.replace(reg, '');
+      }
+      /*
+       * toggleClass可以用来切换一个类
+       * 	如果元素中具有该类，则删除
+       * 	如果元素中没有该类，则添加
+       */
+      function toggleClass(obj, cn) {
+        //判断obj中是否含有cn
+        // if (hasClass(obj, cn)) {
+        //   //有，则删除
+        //   removeClass(obj, cn);
+        // } else {
+        //   //没有，则添加
+        //   addClass(obj, cn);
+        // }
+
+        hasClass(obj, cn) ? removeClass(obj, cn) : addClass(obj, cn);
+      }
