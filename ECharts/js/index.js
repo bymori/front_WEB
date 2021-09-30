9663235425e8339b6429deb84bba62bcefa409f9
by_mori <by_mori@163.com>
2021-09-30 14:25:50 +0800

    饼形图

--- ECharts/js/index.js
+++ ECharts/js/index.js
@@ -4,7 +4,7 @@
  * @Author: by_mori
  * @Date: 2021-09-29 23:13:53
  * @LastEditors: by_mori
- * @LastEditTime: 2021-09-30 12:03:45
+ * @LastEditTime: 2021-09-30 14:19:41
  */
 // 柱状图1模块
 (function () {
@@ -352,5 +352,347 @@
     option.series[1].data = obj.data[1];
     // 需要重新渲染
     myChart.setOption(option);
+  });
+})();
+
+//折线图2模块
+(function () {
+  // 1. 实例化对象
+  var myChart = echarts.init(document.querySelector('.line1 .chart'));
+
+  // 2. 指定配置和数据
+  var option = {
+    tooltip: {
+      trigger: 'axis',
+    },
+    legend: {
+      top: '0%',
+      // data: ['邮件营销', '联盟广告', '视频广告', '直接访问', '搜索引擎'],
+      textStyle: {
+        color: 'rgba(255,255,255,.5)',
+        fontSize: '12',
+      },
+    },
+    grid: {
+      left: '10',
+      top: '30',
+      right: '10',
+      bottom: '10',
+      containLabel: true,
+    },
+    xAxis: [
+      {
+        type: 'category',
+        boundaryGap: false,
+        // x轴更换数据
+        data: [
+          '01',
+          '02',
+          '03',
+          '04',
+          '05',
+          '06',
+          '07',
+          '08',
+          '09',
+          '10',
+          '11',
+          '12',
+          '13',
+          '14',
+          '15',
+          '16',
+          '17',
+          '18',
+          '19',
+          '20',
+          '21',
+          '22',
+          '23',
+          '24',
+          '25',
+          '26',
+          '26',
+          '28',
+          '29',
+          '30',
+        ],
+        // 文本颜色为rgba(255,255,255,.6)  文字大小为 12
+        axisLabel: {
+          textStyle: {
+            color: 'rgba(255,255,255,.6)',
+            fontSize: 12,
+          },
+        },
+        // x轴线的颜色为   rgba(255,255,255,.2)
+        axisLine: {
+          lineStyle: {
+            color: 'rgba(255,255,255,.2)',
+          },
+        },
+      },
+    ],
+    yAxis: [
+      {
+        type: 'value',
+        axisTick: { show: false },
+        axisLine: {
+          lineStyle: {
+            color: 'rgba(255,255,255,.1)',
+          },
+        },
+        axisLabel: {
+          textStyle: {
+            color: 'rgba(255,255,255,.6)',
+            fontSize: 12,
+          },
+        },
+        // 修改分割线的颜色
+        splitLine: {
+          lineStyle: {
+            color: 'rgba(255,255,255,.1)',
+          },
+        },
+      },
+    ],
+    series: [
+      {
+        name: '邮件营销',
+        type: 'line',
+        smooth: true,
+        // 单独修改当前线条的样式
+        lineStyle: {
+          color: '#0184d5',
+          width: '2',
+        },
+        // 填充颜色设置
+        areaStyle: {
+          //渐变色
+          color: new echarts.graphic.LinearGradient(
+            0,
+            0,
+            0,
+            1,
+            [
+              {
+                offset: 0,
+                color: 'rgba(1, 132, 213, 0.4)', // 渐变色的起始颜色
+              },
+              {
+                offset: 0.8,
+                color: 'rgba(1, 132, 213, 0.1)', // 渐变线的结束颜色
+              },
+            ],
+            false
+          ),
+          shadowColor: 'rgba(0, 0, 0, 0.1)',
+        },
+        // 设置拐点
+        symbol: 'circle',
+        // 拐点大小
+        symbolSize: 8,
+        // 开始不显示拐点， 鼠标经过显示
+        showSymbol: false,
+        // 设置拐点颜色以及边框
+        itemStyle: {
+          color: '#0184d5',
+          borderColor: 'rgba(221, 220, 107, .1)',
+          borderWidth: 12,
+        },
+        data: [
+          30, 40, 30, 40, 30, 40, 30, 60, 20, 40, 30, 40, 30, 40, 30, 40, 30,
+          60, 20, 40, 30, 40, 30, 40, 30, 40, 20, 60, 50, 40,
+        ],
+      },
+      {
+        name: '联盟广告',
+        type: 'line',
+        smooth: true,
+        lineStyle: {
+          normal: {
+            color: '#00d887',
+            width: 2,
+          },
+        },
+        areaStyle: {
+          normal: {
+            color: new echarts.graphic.LinearGradient(
+              0,
+              0,
+              0,
+              1,
+              [
+                {
+                  offset: 0,
+                  color: 'rgba(0, 216, 135, 0.4)',
+                },
+                {
+                  offset: 0.8,
+                  color: 'rgba(0, 216, 135, 0.1)',
+                },
+              ],
+              false
+            ),
+            shadowColor: 'rgba(0, 0, 0, 0.1)',
+          },
+        },
+        // 设置拐点 小圆点
+        symbol: 'circle',
+        // 拐点大小
+        symbolSize: 5,
+        // 设置拐点颜色以及边框
+        itemStyle: {
+          color: '#00d887',
+          borderColor: 'rgba(221, 220, 107, .1)',
+          borderWidth: 12,
+        },
+        // 开始不显示拐点， 鼠标经过显示
+        showSymbol: false,
+        data: [
+          130, 10, 20, 40, 30, 40, 80, 60, 20, 40, 90, 40, 20, 140, 30, 40, 130,
+          20, 20, 40, 80, 70, 30, 40, 30, 120, 20, 99, 50, 20,
+        ],
+      },
+    ],
+  };
+
+  // 3. 把配置给实例对象
+  myChart.setOption(option);
+  // 4. 让图表跟随屏幕自动的去适应
+  window.addEventListener('resize', function () {
+    myChart.resize();
+  });
+})();
+
+// 饼形图2
+(function () {
+  // 1. 实例化对象
+  var myChart = echarts.init(document.querySelector('.pie .chart'));
+
+  // 2. 指定配置和数据
+  var option = {
+    color: ['#065aab', '#066eab', '#0682ab', '#0696ab', '#06a0ab'],
+    tooltip: {
+      trigger: 'item',
+      formatter: '{a} <br/>{b}: {c} ({d}%)',
+    },
+    legend: {
+      bottom: '0%',
+      // orient: 'vertical',
+      // 修改小图标的大小
+      itemWidth: 10,
+      itemHeight: 10,
+      // data: ['直接访问', '邮件营销', '联盟广告', '视频广告', '搜索引擎'],
+      // 修改图例组件的文字为 12px
+      textStyle: {
+        color: 'rgba(255,255,255,.5)',
+        fontSize: '12',
+      },
+    },
+    series: [
+      {
+        name: '年龄分布',
+        type: 'pie',
+        // 这个radius可以修改饼形图的大小
+        // radius 第一个值是内圆的半径 第二个值是外圆的半径
+        radius: ['40%', '60%'],
+        center: ['50%', '40%'],
+        avoidLabelOverlap: false,
+        // 图形上的文字
+        label: {
+          show: false,
+          position: 'center',
+        },
+
+        // 链接文字和图形的线是否显示
+        labelLine: {
+          show: false,
+        },
+        data: [
+          { value: 1, name: '0岁以下' },
+          { value: 4, name: '20-29岁' },
+          { value: 2, name: '30-39岁' },
+          { value: 2, name: '40-49岁' },
+          { value: 1, name: '50岁以上' },
+        ],
+      },
+    ],
+  };
+
+  // 3. 把配置给实例对象
+  myChart.setOption(option);
+  // 4. 让图表跟随屏幕自动的去适应
+  window.addEventListener('resize', function () {
+    myChart.resize();
+  });
+})();
+
+// 饼形图2
+(function () {
+  // 1. 实例化对象
+  var myChart = echarts.init(document.querySelector('.pie1 .chart'));
+
+  // 2. 指定配置和数据
+  var option = {
+    color: [
+      '#006cff',
+      '#60cda0',
+      '#ed8884',
+      '#ff9f7f',
+      '#0096ff',
+      '#9fe6b8',
+      '#32c5e9',
+      '#1d9dff',
+    ],
+    tooltip: {
+      trigger: 'item',
+      formatter: '{a} <br/>{b} : {c} ({d}%)',
+    },
+    legend: {
+      bottom: '0%',
+      itemWidth: 10,
+      itemHeight: 10,
+      textStyle: {
+        color: 'rgba(255,255,255,.5)',
+        fontSize: '12',
+      },
+    },
+    series: [
+      {
+        name: '地区分布',
+        type: 'pie',
+        radius: ['10%', '70'],
+        center: ['50%', '45%'],
+        roseType: 'radius',
+        // 图形的文字标签
+        label: {
+          fontSize: 10,
+        },
+        // 链接图形和文字的线条
+        labelLine: {
+          // length 链接图形的线条
+          length: 6,
+          // length2 链接文字的线条
+          length2: 8,
+        },
+        data: [
+          { value: 20, name: '云南' },
+          { value: 26, name: '北京' },
+          { value: 24, name: '山东' },
+          { value: 25, name: '河北' },
+          { value: 20, name: '江苏' },
+          { value: 25, name: '浙江' },
+          { value: 30, name: '四川' },
+          { value: 42, name: '湖北' },
+        ],
+      },
+    ],
+  };
+
+  // 3. 把配置给实例对象
+  myChart.setOption(option);
+  // 4. 让图表跟随屏幕自动的去适应
+  window.addEventListener('resize', function () {
+    myChart.resize();
   });
 })();
