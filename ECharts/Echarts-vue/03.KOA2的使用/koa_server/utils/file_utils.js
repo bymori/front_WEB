e6c3847bb06ed72a39f3efc73bbab04610c05bab
by_mori <by_mori@163.com>
2021-10-01 17:01:12 +0800

    KOA后台

---
+++ ECharts/Echarts-vue/03.KOA2的使用/koa_server/utils/file_utils.js
@@ -0,0 +1,24 @@
+/*
+ * @Descripttion:
+ * @version:
+ * @Author: by_mori
+ * @Date: 2021-10-01 16:16:06
+ * @LastEditors: by_mori
+ * @LastEditTime: 2021-10-01 16:47:25
+ */
+// 读取文件的工具方法
+const fs = require('fs');
+module.exports.getFileJsonData = (filePath) => {
+  // 根据文件的路径, 读取文件的内容
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, 'utf-8', (error, data) => {
+      if (error) {
+        // 读取文件失败
+        reject(error);
+      } else {
+        // 读取文件成功
+        resolve(data);
+      }
+    });
+  });
+};
