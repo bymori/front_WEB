2d464fe752dbd5cd5de1f2ff9742de19735732ff
by_mori <by_mori@163.com>
2021-10-02 16:09:13 +0800

    koa_server

---
+++ ECharts/Echarts-vue/04.vue-ECharts/koa_server/middleware/koa_response_header.js
@@ -0,0 +1,17 @@
+/*
+ * @Descripttion:
+ * @version:
+ * @Author: by_mori
+ * @Date: 2021-10-01 16:16:27
+ * @LastEditors: by_mori
+ * @LastEditTime: 2021-10-01 16:59:35
+ */
+// 设置响应头的中间件
+module.exports = async (ctx, next) => {
+  const contentType = 'application/json; charset=utf-8';
+  ctx.set('Content-Type', contentType);
+  // ctx.response.body = '{"success":true}';
+  ctx.set('Access-Control-Allow-Origin', '*');
+  ctx.set('Access-Control-Allow-Methods', 'OPTIONS, GET, PUT, POST, DELETE');
+  await next();
+};
