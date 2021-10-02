44f37022f58c224659b270286c175980dce683d1
by_mori <by_mori@163.com>
2021-10-02 12:23:06 +0800

    Rank

--- ECharts/Echarts-vue/04.vue-ECharts/vision/src/components/Rank.vue
+++ ECharts/Echarts-vue/04.vue-ECharts/vision/src/components/Rank.vue
@@ -4,7 +4,7 @@
  * @Author: by_mori
  * @Date: 2021-10-02 01:11:37
  * @LastEditors: by_mori
- * @LastEditTime: 2021-10-02 11:11:35
+ * @LastEditTime: 2021-10-02 12:21:15
 -->
 <template>
   <div class="com-container">
@@ -18,6 +18,9 @@ export default {
     return {
       chartInstance: null,
       allData: null,
+      startValue: 0, // 区域缩放的起点值
+      endValue: 9, // 区域缩放的终点值
+      timerId: null // 定时器的标识
     }
   },
   mounted () {
@@ -29,6 +32,10 @@ export default {
   destroyed () {
     window.removeEventListener('resize', this.screenAdapter)
   },
+  destroyed () {
+    clearInterval(this.timerId)
+    window.removeEventListener('resize', this.screenAdapter)
+  },
   methods: {
 
     initChart () {
@@ -36,32 +43,143 @@ export default {
       this.chartInstance = this.$echarts.init(this.$refs.rank_ref, 'chalk')
       /** @type EChartsOption */
       const initOption = {
-
+        title: {
+          text: '▎ 地区销售排行',
+          left: 20,
+          top: 20
+        },
+        tooltip: {
+          show: true
+        },
+        grid: {
+          top: '40%',
+          left: '5%',
+          right: '5%',
+          bottom: '5%',
+          containLabel: true
+        },
+        xAxis: {
+          type: 'category'
+        },
+        yAxis: {
+          type: 'value'
+        },
+        series: [
+          {
+            type: 'bar'
+          }
+        ]
       }
       this.chartInstance.setOption(initOption)
+
+      // 对图表对象进行鼠标事件的监听
+      this.chartInstance.on('mouseover', () => {
+        clearInterval(this.timerId)
+      })
+      this.chartInstance.on('mouseout', () => {
+        this.startInterval()
+      })
     },
     async getData () {
       //获取服务器的数据，对this.allData进行赋值之后，调用updateChart方法更新图表
-      //    const { data: ret } = await this.$http.get('mm')
-      // this.allData = ret
-
+      const { data: ret } = await this.$http.get('rank')
+      this.allData = ret
+      console.log(this.allData);
+      // 对allData里面的每一个元素进行排序, 从大到小进行
+      this.allData.sort((a, b) => {
+        return b.value - a.value
+      })
       this.updateChart()
+      this.startInterval()
     },
     updateChart () {
+      const colorArr = [
+        ['#0BA82C', '#4FF778'],
+        ['#2E72BF', '#23E5E5'],
+        ['#5052EE', '#AB6EE5']
+      ]
       //处理图表需要的数据
+      const provinceArr = this.allData.map(item => {
+        return item.name
+      })
+      const valueArr = this.allData.map(item => {
+        return item.value
+      })
       /** @type EChartsOption */
       const dataOption = {
+        xAxis: {
+          data: provinceArr,
+        },
+        dataZoom: {
+          show: false,
+          startValue: this.startValue,
+          endValue: this.endValue
+        },
+        series: [
+          {
+            data: valueArr,
+            itemStyle: {
+              color: (arg) => {
+                let targetColorArr = null
+                if (arg.value > 300) {
+                  targetColorArr = colorArr[0]
+                } else if (arg.value > 200) {
+                  targetColorArr = colorArr[1]
+                } else {
+                  targetColorArr = colorArr[2]
+                }
+                return new this.$echarts.graphic.LinearGradient(0, 0, 0, 1, [
+                  {
+                    offset: 0,
+                    color: targetColorArr[0]
+                  },
+                  {
+                    offset: 1,
+                    color: targetColorArr[1]
+                  },
+                ])
+              }
+            }
+          }
+        ],
 
       }
       this.chartInstance.setOption(dataOption)
     },
     screenAdapter () {
-      // 当浏览器的大小发生变化的时候, 会调用的方法, 来完成屏幕的适配
+      const titleFontSize = this.$refs.rank_ref.offsetWidth / 100 * 3.6
+      /** @type EChartsOption */
       const adapterOption = {
-
+        title: {
+          textStyle: {
+            fontSize: titleFontSize,
+          },
+        },
+        series: [
+          {
+            barWidth: titleFontSize,
+            itemStyle: {
+              barBorderRadius: [titleFontSize / 2, titleFontSize / 2, 0, 0]
+            }
+          }
+        ]
       }
       this.chartInstance.setOption(adapterOption)
       this.chartInstance.resize()
+    },
+    startInterval () {
+      if (this.timerId) {
+        clearInterval(this.timerId)
+      }
+      this.timerId = setInterval(() => {
+        this.startValue++
+        this.endValue++
+        if (this.endValue > this.allData.length - 1) {
+          this.startValue = 0
+          this.endValue = 9
+        }
+        this.updateChart()
+      }, 2000)
     }
   }
 }
