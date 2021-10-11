f20db087c0bda848256a1eb6bc590d31314a775f
by_mori <by_mori@163.com>
2021-10-11 17:02:09 +0800

    vite的对css，less处理

---
+++ Vuejs/02.LEARN_WEBPACK/10.vite的基本使用/postcss.config.js
@@ -0,0 +1,11 @@
+/*
+ * @Descripttion:
+ * @version:
+ * @Author: by_mori
+ * @Date: 2021-10-11 17:00:25
+ * @LastEditors: by_mori
+ * @LastEditTime: 2021-10-11 17:00:25
+ */
+module.exports = {
+  plugins: [require('postcss-preset-env')],
+};
--- Vuejs/02.LEARN_WEBPACK/10.vite的基本使用/src/main.js
+++ Vuejs/02.LEARN_WEBPACK/10.vite的基本使用/src/main.js
@@ -4,15 +4,22 @@
  * @Author: by_mori
  * @Date: 2021-10-11 16:24:16
  * @LastEditors: by_mori
- * @LastEditTime: 2021-10-11 16:45:38
+ * @LastEditTime: 2021-10-11 16:55:52
  */
 import _ from 'lodash-es';
 // import _ from '../node_modules/lodash-es/lodash.default.js';
 
 // import { sum } from './js/math.js';
 import { sum } from './js/math.js';
+import './css/style.css';
+import './css/title.less';
 
 console.log('hello world vite');
 console.log(sum(20, 30));
 
 console.log(_.join(['abc', 'nba'], '-'));
+
+const titleEl = document.createElement('div');
+titleEl.className = 'title';
+titleEl.innerHTML = 'Hello Vite';
+document.body.appendChild(titleEl);
