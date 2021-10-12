62d569a7d5cd7d37951252820fba380ea1773611
by_mori <by_mori@163.com>
2021-10-13 00:17:27 +0800

    动画的基本使用

--- Vuejs/04_learn_animation/src/main.js
+++ Vuejs/04_learn_animation/src/main.js
@@ -1,4 +1,12 @@
+/*
+ * @Descripttion: 
+ * @version: 
+ * @Author: by_mori
+ * @Date: 2021-10-12 23:56:49
+ * @LastEditors: by_mori
+ * @LastEditTime: 2021-10-12 23:58:51
+ */
 import { createApp } from 'vue'
-import App from './App.vue'
+import App from './01_动画的基本使用/App.vue';
 
 createApp(App).mount('#app')
