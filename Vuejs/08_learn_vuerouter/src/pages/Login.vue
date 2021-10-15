c9da1b0f14eb68a466fdb2fe42c07e6c8f83adae
by_mori <by_mori@163.com>
2021-10-15 17:13:08 +0800

    导航守卫

---
+++ Vuejs/08_learn_vuerouter/src/pages/Login.vue
@@ -0,0 +1,40 @@
+<!--
+ * @Descripttion: 
+ * @version: 
+ * @Author: by_mori
+ * @Date: 2021-10-15 17:00:22
+ * @LastEditors: by_mori
+ * @LastEditTime: 2021-10-15 17:04:50
+-->
+<template>
+  <div>
+    <button @click="loginClick">登录</button>
+  </div>
+</template>
+
+<script>
+import { useRouter } from 'vue-router';
+
+export default {
+  setup () {
+    const router = useRouter();
+
+    const loginClick = () => {
+
+      window.localStorage.setItem("token", "ioinn")
+
+      router.push({
+        path: "/home"
+      })
+    }
+
+    return {
+      loginClick
+    }
+  }
+}
+</script>
+
+
+<style scoped>
+</style>
--- Vuejs/08_learn_vuerouter/src/router/index.js
+++ Vuejs/08_learn_vuerouter/src/router/index.js
@@ -4,7 +4,7 @@
  * @Author: by_mori
  * @Date: 2021-10-15 11:35:49
  * @LastEditors: by_mori
- * @LastEditTime: 2021-10-15 16:28:44
+ * @LastEditTime: 2021-10-15 17:03:35
  */
 import {
   createRouter,
@@ -47,6 +47,11 @@ const routes = [
     component: () => import('../pages/User.vue'),
   },
   {
+    path: '/login',
+    home: 'login',
+    component: () => import('../pages/Login.vue'),
+  },
+  {
     path: '/:pathMatch(.*)',
     component: () => import('../pages/NotFound.vue'),
   },
@@ -79,6 +84,30 @@ router.addRoute({
   children: [
     { path: 'moment', component: () => import('../pages/HomeMoment.vue') },
   ],
+});
+
+// 导航守卫beforeEach
+let counter = 0;
+// to: Route对象, 即将跳转到的Route对象
+// from: Route对象,
+/**
+ * 返回值问题:
+ *    1.false: 不进行导航
+ *    2.undefined或者不写返回值: 进行默认导航
+ *    3.字符串: 路径, 跳转到对应的路径中
+ *    4.对象: 类似于 router.push({path: "/login", query: ....})
+ */
+router.beforeEach((to, from) => {
+  console.log(`进行了${++counter}路由跳转`);
+  //   if (to.path.indexOf('/home') !== -1) {
+  //     return '/login';
+  //   }
+  if (to.path !== '/login') {
+    const token = window.localStorage.getItem('token');
+    if (!token) {
+      return '/login';
+    }
+  }
 });
 
 export default router;
