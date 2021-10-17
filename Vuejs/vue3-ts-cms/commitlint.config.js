a9fc8ac4c8f414400673a45420c3a5456faa3d4f
by_mori <by_mori@163.com>
2021-10-17 18:00:24 +0800

    test

---
+++ Vuejs/vue3-ts-cms/commitlint.config.js
@@ -0,0 +1,11 @@
+/*
+ * @Descripttion:
+ * @version:
+ * @Author: by_mori
+ * @Date: 2021-10-17 17:57:15
+ * @LastEditors: by_mori
+ * @LastEditTime: 2021-10-17 17:57:16
+ */
+module.exports = {
+  extends: ['@commitlint/config-conventional']
+}
