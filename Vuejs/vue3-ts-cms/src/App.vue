f1c1dcd7ead42523c23bf40dee57756dcae83b47
by_mori <by_mori@163.com>
2021-10-17 17:21:07 +0800

    Keep calm and commit

--- Vuejs/vue3-ts-cms/src/App.vue
+++ Vuejs/vue3-ts-cms/src/App.vue
@@ -17,8 +17,8 @@
 
 <style lang="less">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
+      font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
@@ -27,7 +27,7 @@
 #nav {
   padding: 30px;
 
-  a {
+      a {
     font-weight: bold;
     color: #2c3e50;
 
