131f52ada6f7ca85fbaab5952d5081ad96328ff2
by_mori <by_mori@163.com>
2021-10-17 16:47:35 +0800

    test

---
+++ Vuejs/vue3-ts-cms/src/main.ts
@@ -0,0 +1,12 @@
+/*
+ * @Descripttion:
+ * @version:
+ * @Author: by_mori
+ * @Date: 2021-10-17 14:35:25
+ * @LastEditors: by_mori
+ * @LastEditTime: 2021-10-17 16:17:47
+ */
+import { createApp } from 'vue';
+import App from './App.vue';
+
+createApp(App).mount('#app');
