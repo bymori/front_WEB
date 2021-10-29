e98695e484bc599c59ad6efaac4d7ac3c35361e0
by_mori <by_mori@163.com>
2021-10-29 22:12:07 +0800

    NodeJs 封装静态WEB服务

---
+++ Nodejs/00_Nodejs+Express+Monggose/demo09/02_app_async.js
@@ -0,0 +1,46 @@
+/*
+ * @Descripttion:
+ * @version:
+ * @Author: by_mori
+ * @Date: 2021-10-29 21:27:47
+ * @LastEditors: by_mori
+ * @LastEditTime: 2021-10-29 22:08:58
+ */
+
+// async await处理异步读取文件  getFileMime
+
+const http = require('http');
+const fs = require('fs');
+const common = require('./module/common.js');
+const path = require('path');
+const url = require('url');
+
+http
+  .createServer(function (req, res) {
+    //http://127.0.0.1:8081/login.html
+    //http://127.0.0.1:8081/index.html
+
+    //1、获取地址
+    // let pathname = req.url;
+    let pathname = url.parse(req.url).pathname;
+    pathname = pathname == '/' ? '/index.html' : pathname;
+
+    //可以获取后缀名path.extname()
+    let extname = path.extname(pathname);
+
+    //2、通过fs模块读取文件
+    if (pathname != '/favicon.ico') {
+      fs.readFile('./static' + pathname, async (err, data) => {
+        if (err) {
+          res.writeHead(404, { 'Content-Type': 'text/html;charset="utf-8"' });
+          res.end('404这个页面不存在');
+        }
+        let mime = await common.getFileMime(extname);
+        res.writeHead(200, { 'Content-Type': '' + mime + ';charset="utf-8"' });
+        res.end(data);
+      });
+    }
+  })
+  .listen(8081);
+
+console.log('Server running at http://127.0.0.1:8081/');
