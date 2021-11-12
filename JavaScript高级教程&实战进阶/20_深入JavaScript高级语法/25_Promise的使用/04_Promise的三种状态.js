1d543e8238b1e8508c1267a0167f46d605318b24
by_mori <by_mori@163.com>
2021-11-13 01:25:30 +0800

    Promise的使用

---
+++ JavaScript高级教程&实战进阶/20_深入JavaScript高级语法/25_Promise的使用/04_Promise的三种状态.js
@@ -0,0 +1,31 @@
+/*
+ * @Descripttion: io小栈 —— ioinn.cn
+ * @version: 0.0.1
+ * @Author: by_mori
+ * @Date: 2021-11-13 01:03:45
+ * @LastEditors: by_mori
+ * @LastEditTime: 2021-11-13 01:03:45
+ */
+// const promise = new Promise((resolve, reject) => {});
+
+// promise.then(
+//   (res) => {},
+//   (err) => {}
+// );
+
+// 完全等价于下面的代码
+// 注意: Promise状态一旦确定下来, 那么就是不可更改的(锁定)
+new Promise((resolve, reject) => {
+  // pending状态: 待定/悬而未决的
+  console.log('--------');
+  reject(); // 处于rejected状态(已拒绝状态)
+  resolve(); // 处于fulfilled状态(已敲定/兑现状态)
+  console.log('++++++++++++');
+}).then(
+  (res) => {
+    console.log('res:', res);
+  },
+  (err) => {
+    console.log('err:', err);
+  }
+);
