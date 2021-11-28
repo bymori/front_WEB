ae37909d54e7a1009d910698e02c05c761c207d3
by_mori <by_mori@163.com>
2021-11-28 11:24:49 +0800

    create-react-app

---
+++ React/React_Basics/02_learn_scaffold/src/App.js
@@ -0,0 +1,33 @@
+/*
+ * @Descripttion: io小栈 —— ioinn.cn
+ * @version: 0.0.1
+ * @Author: by_mori
+ * @Date: 2021-11-28 11:15:58
+ * @LastEditors: by_mori
+ * @LastEditTime: 2021-11-28 11:20:25
+ */
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer">
+          Learn React
+        </a>
+        <h3>哈喽 沫沫</h3>
+      </header>
+    </div>
+  );
+}
+
+export default App;
