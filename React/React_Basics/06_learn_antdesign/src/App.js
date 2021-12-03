14543bb508639c55efe5f2d5f69149b6414b4122
by_mori <by_mori@163.com>
2021-12-03 21:21:37 +0800

    learn_antdesign

---
+++ React/React_Basics/06_learn_antdesign/src/App.js
@@ -0,0 +1,15 @@
+/*
+ * @Descripttion: io小栈 —— ioinn.cn
+ * @version: 0.0.1
+ * @Author: by_mori
+ * @Date: 2021-12-03 16:54:31
+ * @LastEditors: by_mori
+ * @LastEditTime: 2021-12-03 16:54:31
+ */
+import React, { PureComponent } from 'react';
+
+export default class App extends PureComponent {
+  render() {
+    return <div>a</div>;
+  }
+}
---
+++ React/React_Basics/06_learn_antdesign/src/index.js
@@ -0,0 +1,13 @@
+/*
+ * @Descripttion: io小栈 —— ioinn.cn
+ * @version: 0.0.1
+ * @Author: by_mori
+ * @Date: 2021-12-03 15:51:58
+ * @LastEditors: by_mori
+ * @LastEditTime: 2021-12-03 16:54:11
+ */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
