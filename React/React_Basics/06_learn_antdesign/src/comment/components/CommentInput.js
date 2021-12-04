68bbf1d0a16327bb69e988b964deead435361326
by_mori <by_mori@163.com>
2021-12-04 20:32:05 +0800

    antd

--- React/React_Basics/06_learn_antdesign/src/comment/components/CommentInput.js
+++ React/React_Basics/06_learn_antdesign/src/comment/components/CommentInput.js
@@ -4,12 +4,58 @@
  * @Author: by_mori
  * @Date: 2021-12-04 17:10:39
  * @LastEditors: by_mori
- * @LastEditTime: 2021-12-04 17:10:39
+ * @LastEditTime: 2021-12-04 19:37:41
  */
 import React, { PureComponent } from 'react';
+import moment from 'moment';
+
+import { Input, Button } from 'antd';
 
 export default class CommentInput extends PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      content: '',
+    };
+  }
+
   render() {
-    return <div>b</div>;
+    return (
+      <div>
+        <Input.TextArea
+          rows={4}
+          value={this.state.content}
+          onChange={(e) => this.handleChange(e)}
+        />
+        <Button type="primary" onClick={(e) => this.addComment()}>
+          添加评论
+        </Button>
+      </div>
+    );
+  }
+
+  handleChange(event) {
+    this.setState({
+      content: event.target.value,
+    });
+  }
+
+  addComment() {
+    const commentInfo = {
+      id: moment().valueOf(),
+      avatar:
+        'https://cdn.jsdelivr.net/gh/bymori/image-PicX@main/blog/image.69uvm447wr80.png',
+      nickname: 'momo',
+      dateTime: moment(),
+      content: this.state.content,
+      comments: [],
+    };
+
+    this.props.submitComment(commentInfo);
+
+    this.setState({
+      content: '',
+    });
   }
 }
--- React/React_Basics/06_learn_antdesign/src/comment/components/CommentItem.js
+++ React/React_Basics/06_learn_antdesign/src/comment/components/CommentItem.js
@@ -4,12 +4,38 @@
  * @Author: by_mori
  * @Date: 2021-12-04 17:10:51
  * @LastEditors: by_mori
- * @LastEditTime: 2021-12-04 17:10:51
+ * @LastEditTime: 2021-12-04 20:25:32
  */
 import React, { PureComponent } from 'react';
 
+import { Comment, Tooltip, Avatar } from 'antd';
+import { DeleteOutlined } from '@ant-design/icons';
+
 export default class CommentItem extends PureComponent {
   render() {
-    return <div>c</div>;
+    const { nickname, avatar, content, dateTime } = this.props.comment;
+
+    return (
+      <Comment
+        author={<a href="/#">{nickname}</a>}
+        avatar={<Avatar src={avatar} alt={nickname} />}
+        content={<p>{content}</p>}
+        datetime={
+          <Tooltip title={dateTime.format('YYYY-MM-DD HH:mm:ss')}>
+            <span>{dateTime.fromNow()}</span>
+          </Tooltip>
+        }
+        actions={[
+          <span onClick={(e) => this.removeItem()}>
+            <DeleteOutlined />
+            删除
+          </span>,
+        ]}
+      />
+    );
+  }
+
+  removeItem() {
+    this.props.removeItem();
   }
 }
