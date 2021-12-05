4429c2a535a57b5c26b7eaf00b74aec7662d6451
by_mori <by_mori@163.com>
2021-12-05 10:42:30 +0800

    axios

--- React/React_Basics/07_learn_axios/src/App.js
+++ React/React_Basics/07_learn_axios/src/App.js
@@ -4,7 +4,7 @@
  * @Author: by_mori
  * @Date: 2021-12-04 21:26:20
  * @LastEditors: by_mori
- * @LastEditTime: 2021-12-04 22:45:31
+ * @LastEditTime: 2021-12-05 10:40:50
  */
 import React, { PureComponent } from 'react';
 
@@ -82,6 +82,52 @@ export default class App extends PureComponent {
     // } catch (err) {
     //   console.log(err);
     // }
+    //
+    // 5.axios.all === Promise.all
+    // const request1 = axios({
+    //   // url: 'https://httpbin.org/get',
+    //   url: '/get',
+    //   params: { name: 'momo', age: 18 },
+    // });
+    // const request2 = axios({
+    //   // url: 'https://httpbin.org/post',
+    //   url: '/post',
+    //   data: { name: 'mori', age: 40 },
+    //   method: 'post',
+    // });
+    // axios.all([request1, request2]).then(([res1, res2]) => {
+    //   console.log(res1, res2);
+    // });
+    //
+    // 6.Promise.all
+    // const promise1 = new Promise((resolve, reject) => {
+    //   setTimeout(() => {
+    //     resolve('promise1');
+    //   }, 1000);
+    // });
+    // const promise2 = new Promise((resolve, reject) => {
+    //   setTimeout(() => {
+    //     resolve('promise2');
+    //   }, 3000);
+    // });
+    // Promise.all([promise1, promise2]).then((res) => {
+    //   console.log(res);
+    // });
+    //
+    // 7. index.js  默认配置
+    //
+    // 8.创建新的实例
+    const instance1 = axios.create({
+      baseURL: 'http://ioinn.cn',
+      timeout: 5000,
+      headers: {},
+    });
+
+    const instance2 = axios.create({
+      baseURL: 'http://baidu.com',
+      timeout: 10000,
+      headers: {},
+    });
   }
 
   render() {
--- React/React_Basics/07_learn_axios/src/index.js
+++ React/React_Basics/07_learn_axios/src/index.js
@@ -4,11 +4,19 @@
  * @Author: by_mori
  * @Date: 2021-12-04 20:41:34
  * @LastEditors: by_mori
- * @LastEditTime: 2021-12-04 21:26:15
+ * @LastEditTime: 2021-12-05 10:33:08
  */
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import axios from 'axios';
+
 import App from './App';
+
+// 7.默认配置
+axios.defaults.baseURL = 'https://httpbin.org';
+axios.defaults.timeout = 5000;
+axios.defaults.headers.common['token'] = 'tokentokentoken';
+// axios.defaults.headers.post['Content-Type'] = 'application/text';
 
 ReactDOM.render(<App />, document.getElementById('root'));
