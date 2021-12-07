096ea74bb2ff376b3d1825b743cf54a6641d4b3b
by_mori <by_mori@163.com>
2021-12-07 10:58:24 +0800

    react-redux

--- React/React_Basics/10_react-redux/src/index.js
+++ React/React_Basics/10_react-redux/src/index.js
@@ -4,20 +4,22 @@
  * @Author: by_mori
  * @Date: 2021-12-06 11:19:01
  * @LastEditors: by_mori
- * @LastEditTime: 2021-12-06 22:49:10
+ * @LastEditTime: 2021-12-07 10:45:34
  */
 import React from 'react';
 import ReactDOM from 'react-dom';
 
 import store from './store';
 
-import { StoreContext } from './utils/context';
+// import { StoreContext } from './utils/context';
+import { Provider } from 'react-redux';
 
 import App from './App';
 
 ReactDOM.render(
-  <StoreContext.Provider value={store}>
+  // <Provider value={store}>
+  <Provider store={store}>
     <App />
-  </StoreContext.Provider>,
+  </Provider>,
   document.getElementById('root')
 );
