50b3e88ff90c7c32be2e7a3e4efd4b5300b32522
by_mori <by_mori@163.com>
2021-12-10 15:19:16 +0800

    history和hash

---
+++ React/React_Basics/13_learn-router/src/App.js
@@ -0,0 +1,15 @@
+/*
+ * @Descripttion: io小栈 —— ioinn.cn
+ * @version: 0.0.1
+ * @Author: by_mori
+ * @Date: 2021-12-09 20:42:47
+ * @LastEditors: by_mori
+ * @LastEditTime: 2021-12-10 11:52:52
+ */
+import React, { PureComponent } from 'react';
+
+export default class App extends PureComponent {
+  render() {
+    return <div>aa</div>;
+  }
+}
---
+++ React/React_Basics/13_learn-router/src/index.js
@@ -0,0 +1,19 @@
+/*
+ * @Descripttion: io小栈 —— ioinn.cn
+ * @version: 0.0.1
+ * @Author: by_mori
+ * @Date: 2021-12-09 20:42:47
+ * @LastEditors: by_mori
+ * @LastEditTime: 2021-12-10 11:53:03
+ */
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
