5e4276dfae68ddcf1158c9f4e5d85cb4ad1dc0d9
by_mori <by_mori@163.com>
2021-12-10 21:59:34 +0800

    JumpToJoin

--- React/React_Basics/13_learn-router/src/pages/about.js
+++ React/React_Basics/13_learn-router/src/pages/about.js
@@ -4,10 +4,10 @@
  * @Author: by_mori
  * @Date: 2021-12-10 15:32:46
  * @LastEditors: by_mori
- * @LastEditTime: 2021-12-10 21:03:44
+ * @LastEditTime: 2021-12-10 21:58:09
  */
 import React, { PureComponent } from 'react';
-import { NavLink, Route, Routes } from 'react-router-dom';
+import { NavLink, Route, Routes, useNavigate } from 'react-router-dom';
 
 export function AboutHistory(props) {
   return <h2>企业成立于2000年, 拥有悠久的历史文化 </h2>;
@@ -25,8 +25,24 @@ export function AboutJoin(props) {
   return <h2>投递简历到ioinn@ioinn.cn</h2>;
 }
 
+// function QueryNavLink({ to, ...props }) {
+//   let location = useLocation();
+//   console.log(location);
+//   return <NavLink to={to + location.search} {...props} />;
+// }
+
+function JumpToJoin() {
+  let navigate = useNavigate();
+  function handleClick() {
+    navigate('join');
+  }
+  return <button onClick={handleClick}>加入我们</button>;
+}
+
 export default class About extends PureComponent {
   render() {
+    console.log(this.props.route);
+
     const activeClassName = ({ isActive }) => (isActive ? 'about-active' : '');
 
     return (
@@ -40,9 +56,8 @@ export default class About extends PureComponent {
         <NavLink to="contact" className={activeClassName}>
           联系我们
         </NavLink>
-        <NavLink to="join" className={activeClassName}>
-          联系我们
-        </NavLink>
+        <JumpToJoin />
+        {/* <button onClick={(e) => this.jumpToJoin()}>加入我们</button> */}
 
         <Routes>
           <Route path="history" element={<AboutHistory />} />
@@ -53,4 +68,11 @@ export default class About extends PureComponent {
       </div>
     );
   }
+
+  // jumpToJoin() {
+  //   console.log(this.props.history);
+  //   console.log(this.props.location);
+  //   console.log(this.props.match);
+  //   // this.props.history.push('join');
+  // }
 }
