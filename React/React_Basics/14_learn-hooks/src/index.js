e68d9428b1d7f416fa8822d5893d9fdde0bb1ce9
by_mori <by_mori@163.com>
2021-12-11 18:12:20 +0800

    learn-hooks

---
+++ React/React_Basics/14_learn-hooks/src/index.js
@@ -0,0 +1,18 @@
+/*
+ * @Descripttion: io小栈 —— ioinn.cn
+ * @version: 0.0.1
+ * @Author: by_mori
+ * @Date: 2021-12-11 17:00:29
+ * @LastEditors: by_mori
+ * @LastEditTime: 2021-12-11 18:12:09
+ */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.js';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
