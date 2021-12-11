983f031a4783f68866bd99e029fcb76e1da96746
by_mori <by_mori@163.com>
2021-12-11 21:57:07 +0800

    useState使用

--- React/React_Basics/14_learn-hooks/src/01_体验hooks/03_counter-hook.js
+++ React/React_Basics/14_learn-hooks/src/01_体验hooks/03_counter-hook.js
@@ -4,19 +4,35 @@
  * @Author: by_mori
  * @Date: 2021-12-11 20:00:49
  * @LastEditors: by_mori
- * @LastEditTime: 2021-12-11 20:20:38
+ * @LastEditTime: 2021-12-11 21:56:18
  */
 import React, { useState } from 'react';
 
 export default function CounterHook() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
+  const [count, setCount] = useState(() => 10);
 
   console.log('CounterHook渲染');
+  function handleBtnClick() {
+    // setCount(count - 10);
+    // setCount(count - 10);
+    // setCount(count - 10);
+    // setCount(count - 10);
+
+    setCount((prevCount) => prevCount - 10);
+    setCount((prevCount) => prevCount - 10);
+    setCount((prevCount) => prevCount - 10);
+    setCount((prevCount) => prevCount - 10);
+  }
 
   return (
     <div>
       <h2>当前计数:{count}</h2>
       <button onClick={(e) => setCount(count + 1)}>+1</button>
+      <button onClick={(e) => setCount((prevCount) => prevCount + 10)}>
+        +10
+      </button>{' '}
+      <button onClick={handleBtnClick}>-10</button>
       <button onClick={(e) => setCount(count - 1)}>-1</button>
     </div>
   );
