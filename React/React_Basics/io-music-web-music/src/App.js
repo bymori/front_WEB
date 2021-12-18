217584a4dba3db7c0c53ef749305754e222b22c1
by_mori <by_mori@163.com>
2021-12-18 19:43:10 +0800

    reset.css

---
+++ React/React_Basics/io-music-web-music/src/App.js
@@ -0,0 +1,13 @@
+/*
+ * @Descripttion: io小栈 —— ioinn.cn
+ * @version: 0.0.1
+ * @Author: by_mori
+ * @Date: 2021-12-18 14:43:46
+ * @LastEditors: by_mori
+ * @LastEditTime: 2021-12-18 14:43:46
+ */
+import React, { memo } from 'react';
+
+export default memo(function App() {
+  return <div>a</div>;
+});
---
+++ React/React_Basics/io-music-web-music/src/index.js
@@ -0,0 +1,16 @@
+/*
+ * @Descripttion: io小栈 —— ioinn.cn
+ * @version: 0.0.1
+ * @Author: by_mori
+ * @Date: 2021-12-18 12:07:13
+ * @LastEditors: by_mori
+ * @LastEditTime: 2021-12-18 19:37:24
+ */
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import '../src/assets/css/reset.css';
+
+import App from './App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
