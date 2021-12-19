d023ac0e898d6cbfc0497dbf02a9824612562028
by_mori <by_mori@163.com>
2021-12-19 22:44:10 +0800

    Header

--- React/React_Basics/io-music-web-music/src/components/app-header/index.js
+++ React/React_Basics/io-music-web-music/src/components/app-header/index.js
@@ -4,19 +4,31 @@
  * @Author: by_mori
  * @Date: 2021-12-18 22:59:06
  * @LastEditors: by_mori
- * @LastEditTime: 2021-12-18 23:05:32
+ * @LastEditTime: 2021-12-19 22:43:30
  */
 import React, { memo } from 'react';
 import { NavLink } from 'react-router-dom';
 
+import { HeaderWrapper, HeaderLeft, HeaderRight } from './style';
+
 export default memo(function IOAppHeader() {
   return (
-    <div>
-      <h2>
-        <NavLink to="/">发现音乐</NavLink>
-        <NavLink to="mine/">我的音乐</NavLink>
-        <NavLink to="/friend">我的朋友</NavLink>
-      </h2>
-    </div>
+    <HeaderWrapper>
+      <div className="content wrap-v1">
+        <HeaderLeft>
+          {/* <a href="/" className="logo sprite_01">
+            // 网易云音乐 使用 NavLink 解决 点击a标签时 在 / 页面跳转会刷新
+          </a> */}
+          <NavLink to="/" className="logo sprite_01">
+            网易云音乐
+          </NavLink>
+          <NavLink to="/">发现音乐</NavLink>
+          <NavLink to="mine/">我的音乐</NavLink>
+          <NavLink to="/friend">我的朋友</NavLink>
+        </HeaderLeft>
+        <HeaderRight>HeaderRight</HeaderRight>
+      </div>
+      <div className="divider"></div>
+    </HeaderWrapper>
   );
 });
