30f505b529f5f6e56ff25fa1c0d8e7982239d6f6
by_mori <by_mori@163.com>
2021-12-24 16:27:42 +0800

    歌曲播放 时间获取

--- React/React_Basics/io-music-web-music/src/pages/player/app-player-bar/index.js
+++ React/React_Basics/io-music-web-music/src/pages/player/app-player-bar/index.js
@@ -4,37 +4,59 @@
  * @Author: by_mori
  * @Date: 2021-12-24 13:32:51
  * @LastEditors: by_mori
- * @LastEditTime: 2021-12-24 15:10:47
+ * @LastEditTime: 2021-12-24 16:25:25
  */
-import React, { memo, useEffect } from 'react';
+import React, { memo, useState, useCallback, useEffect, useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
-import { getSizeImage } from '@/utils/format-utils';
+import { getSizeImage, formatDate, getPlaySong } from '@/utils/format-utils';
 
 import { getSongDetailAction } from '../store/actionCreators';
 import { Slider } from 'antd';
 import { PlayerBarWrapper, Control, PlayInfo, Operator } from './style';
 
 export default memo(function IOAppPlayerBar() {
+  // props and state
+  const [currentTime, setCurrentTime] = useState(0);
+
+  // redux hook
   const { currentSong } = useSelector((state) => ({
     currentSong: state.getIn(['player', 'currentSong']),
   }));
 
   const dispatch = useDispatch();
 
+  const audioRef = useRef();
   useEffect(() => {
     dispatch(getSongDetailAction(468517654));
   }, [dispatch]);
 
   // other handle
   const picUrl = (currentSong.al && currentSong.al.picUrl) || '';
+  const singerName = (currentSong.ar && currentSong.ar[0].name) || '未知歌手';
+  const duration = currentSong.dt || 0;
+  const showDuration = formatDate(duration, 'mm:ss');
+  const showCurrentTime = formatDate(currentTime, 'mm:ss');
+  const progress = (currentTime / duration) * 100;
 
+  // handle function
+  const playMusic = useCallback(() => {
+    audioRef.current.src = getPlaySong(currentSong.id);
+    audioRef.current.play();
+  });
+
+  const timeUpdate = (e) => {
+    const currentTime = e.target.currentTime;
+    setCurrentTime(currentTime * 1000);
+  };
   return (
     <PlayerBarWrapper className="sprite_player">
       <div className="content  wrap-v2">
         <Control>
           <button className="sprite_player prev"></button>
-          <button className="sprite_player play"></button>
+          <button
+            className="sprite_player play"
+            onClick={(e) => playMusic()}></button>
           <button className="sprite_player next"></button>
         </Control>
         <PlayInfo>
@@ -45,17 +67,17 @@ export default memo(function IOAppPlayerBar() {
           </div>
           <div className="info">
             <div className="song">
-              <span className="song-name">借(Live)</span>
+              <span className="song-name">{currentSong.name}</span>
               <a href="/" className="singer-name">
-                毛不易
+                {singerName}
               </a>
             </div>
             <div className="progress">
-              <Slider defaultValue={30} />
+              <Slider defaultValue={0} value={progress} />
               <div className="time">
-                <span className="now-time">00:02</span>
+                <span className="now-time">{showCurrentTime}</span>
                 <span className="divider">/</span>
-                <span className="duration">04:35</span>
+                <span className="duration">{showDuration}</span>
               </div>
             </div>
           </div>
@@ -72,6 +94,11 @@ export default memo(function IOAppPlayerBar() {
           </div>
         </Operator>
       </div>
+      <audio
+        ref={audioRef}
+        onTimeUpdate={(e) => timeUpdate(e)}
+        // onEnded={(e) => handleMusicEnded()}
+      />
     </PlayerBarWrapper>
   );
 });
--- React/React_Basics/io-music-web-music/src/utils/format-utils.js
+++ React/React_Basics/io-music-web-music/src/utils/format-utils.js
@@ -4,7 +4,7 @@
  * @Author: by_mori
  * @Date: 2021-12-22 22:25:44
  * @LastEditors: by_mori
- * @LastEditTime: 2021-12-22 22:28:50
+ * @LastEditTime: 2021-12-24 16:02:46
  */
 
 export function getCount(count) {
@@ -20,4 +20,48 @@ export function getCount(count) {
 
 export function getSizeImage(imgUrl, size) {
   return `${imgUrl}?param=${size}x${size}`;
+}
+
+export function formatDate(time, fmt) {
+  let date = new Date(time);
+
+  if (/(y+)/.test(fmt)) {
+    fmt = fmt.replace(
+      RegExp.$1,
+      (date.getFullYear() + '').substr(4 - RegExp.$1.length)
+    );
+  }
+  let o = {
+    'M+': date.getMonth() + 1,
+    'd+': date.getDate(),
+    'h+': date.getHours(),
+    'm+': date.getMinutes(),
+    's+': date.getSeconds(),
+  };
+  for (let k in o) {
+    if (new RegExp(`(${k})`).test(fmt)) {
+      let str = o[k] + '';
+      fmt = fmt.replace(
+        RegExp.$1,
+        RegExp.$1.length === 1 ? str : padLeftZero(str)
+      );
+    }
+  }
+  return fmt;
+}
+
+function padLeftZero(str) {
+  return ('00' + str).substr(str.length);
+}
+
+export function formatMonthDay(time) {
+  return formatDate(time, 'MM月dd日');
+}
+
+export function formatMinuteSecond(time) {
+  return formatDate(time, 'mm:ss');
+}
+
+export function getPlaySong(id) {
+  return `https://music.163.com/song/media/outer/url?id=${id}.mp3`;
 }
