c1a8454e4cf5f13f3c2486333e91b909f9911785
by_mori <by_mori@163.com>
2021-12-24 17:22:00 +0800

    进度条

--- React/React_Basics/io-music-web-music/src/pages/player/app-player-bar/index.js
+++ React/React_Basics/io-music-web-music/src/pages/player/app-player-bar/index.js
@@ -4,7 +4,7 @@
  * @Author: by_mori
  * @Date: 2021-12-24 13:32:51
  * @LastEditors: by_mori
- * @LastEditTime: 2021-12-24 16:25:25
+ * @LastEditTime: 2021-12-24 17:20:40
  */
 import React, { memo, useState, useCallback, useEffect, useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
@@ -18,6 +18,8 @@ import { PlayerBarWrapper, Control, PlayInfo, Operator } from './style';
 export default memo(function IOAppPlayerBar() {
   // props and state
   const [currentTime, setCurrentTime] = useState(0);
+  const [progress, setProgress] = useState(0);
+  const [isChanging, setIsChanging] = useState(false);
 
   // redux hook
   const { currentSong } = useSelector((state) => ({
@@ -37,7 +39,7 @@ export default memo(function IOAppPlayerBar() {
   const duration = currentSong.dt || 0;
   const showDuration = formatDate(duration, 'mm:ss');
   const showCurrentTime = formatDate(currentTime, 'mm:ss');
-  const progress = (currentTime / duration) * 100;
+  //   const progress = (currentTime / duration) * 100;
 
   // handle function
   const playMusic = useCallback(() => {
@@ -46,9 +48,41 @@ export default memo(function IOAppPlayerBar() {
   });
 
   const timeUpdate = (e) => {
-    const currentTime = e.target.currentTime;
-    setCurrentTime(currentTime * 1000);
+    // const currentTime = e.target.currentTime;
+    setCurrentTime(e.target.currentTime * 1000);
+    if (!isChanging) {
+      setProgress((currentTime / duration) * 100);
+    }
   };
+
+  const sliderChange = useCallback(
+    (value) => {
+      setIsChanging(true);
+      const currentTime = (value / 100) * duration;
+      setCurrentTime(currentTime);
+      setProgress(value);
+    },
+    [duration]
+  );
+
+  const sliderAfterChange = useCallback(
+    (value) => {
+      const currentTime = ((value / 100) * duration) / 1000;
+      audioRef.current.currentTime = currentTime;
+      setCurrentTime(currentTime * 1000);
+      setIsChanging(false);
+    },
+    [duration]
+  );
+
+  //   function formatter(value) {
+  // // 废弃命令 sliderChange 设置 currentTime 直接显示 showCurrentTime 只需要设置一次
+  //     const currentTime = (value / 100) * duration;
+  //     const tipFormatter = formatDate(currentTime, 'mm:ss');
+  //     // return `${tipFormatter}`;
+  //     return `${showCurrentTime}`;
+  //   }
+
   return (
     <PlayerBarWrapper className="sprite_player">
       <div className="content  wrap-v2">
@@ -73,7 +107,16 @@ export default memo(function IOAppPlayerBar() {
               </a>
             </div>
             <div className="progress">
-              <Slider defaultValue={0} value={progress} />
+              <Slider
+                defaultValue={0}
+                value={progress}
+                onChange={sliderChange}
+                onAfterChange={sliderAfterChange}
+                // tipFormatter={formatter}
+                tipFormatter={() => {
+                  return `${showCurrentTime}`;
+                }}
+              />
               <div className="time">
                 <span className="now-time">{showCurrentTime}</span>
                 <span className="divider">/</span>
