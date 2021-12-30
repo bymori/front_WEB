fbca806181ae5fa8d37dabd91b629e8924024332
by_mori <by_mori@163.com>
2021-12-30 11:21:25 +0800

    使用antd列表重构PlayList

--- React/React_Basics/io-music-web-music/src/pages/player/app-play-panel/c-cpns/play-list/index.js
+++ React/React_Basics/io-music-web-music/src/pages/player/app-play-panel/c-cpns/play-list/index.js
@@ -4,7 +4,7 @@
  * @Author: by_mori
  * @Date: 2021-12-30 08:44:14
  * @LastEditors: by_mori
- * @LastEditTime: 2021-12-30 10:25:56
+ * @LastEditTime: 2021-12-30 11:20:50
  */
 import React, { memo } from 'react';
 import { useSelector, shallowEqual } from 'react-redux';
@@ -13,6 +13,9 @@ import classNames from 'classnames';
 import { formatMinuteSecond } from '@/utils/format-utils';
 
 import { PlayListWrapper } from './style';
+
+import { List } from 'antd';
+import VirtualList from 'rc-virtual-list';
 
 export default memo(function IOPlayList() {
   const { playList, currentSongIndex } = useSelector(
@@ -23,9 +26,11 @@ export default memo(function IOPlayList() {
     shallowEqual
   );
 
+  const ContainerHeight = 400;
+
   return (
     <PlayListWrapper>
-      {playList.map((item, index) => {
+      {/* {playList.map((item, index) => {
         return (
           <div
             key={item.id}
@@ -40,7 +45,31 @@ export default memo(function IOPlayList() {
             </div>
           </div>
         );
-      })}
+      })} */}
+
+      <List split={false}>
+        <VirtualList
+          data={playList}
+          height={ContainerHeight}
+          itemHeight={47}
+          // onScroll={onScroll}
+        >
+          {(item, index) => (
+            <List.Item
+              key={item.id}
+              className={classNames('play-item', {
+                active: currentSongIndex === index,
+              })}>
+              <div className="left">{item.name}</div>
+              <div className="right">
+                <span className="singer">{item.ar[0].name}</span>
+                <span className="duration">{formatMinuteSecond(item.dt)}</span>
+                <span className="sprite_playlist link"></span>
+              </div>
+            </List.Item>
+          )}
+        </VirtualList>
+      </List>
     </PlayListWrapper>
   );
 });
