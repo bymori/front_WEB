d6fb86b0ad5933e89afe222a7789e78c27c20ff5
by_mori <by_mori@163.com>
2022-01-02 19:40:21 +0800

    mine样式布局

--- React/React_Basics/io-music-web-music/src/pages/mine/index.js
+++ React/React_Basics/io-music-web-music/src/pages/mine/index.js
@@ -4,7 +4,7 @@
  * @Author: by_mori
  * @Date: 2021-12-18 23:18:47
  * @LastEditors: by_mori
- * @LastEditTime: 2022-01-02 15:13:56
+ * @LastEditTime: 2022-01-02 19:39:54
  */
 import React, { memo } from 'react';
 import { NavLink, Outlet, useLocation } from 'react-router-dom';
@@ -33,7 +33,72 @@ export default memo(function IOMine() {
       <div div className="content wrap-v2">
         {userDetail ? (
           <div className="userDetail">
-            <img src={userDetail.profile.backgroundUrl} alt="" />
+            <h1 className="userHeader">
+              <img
+                className="avatar"
+                src={userDetail.profile.avatarUrl}
+                alt=""
+              />
+              {userDetail.profile.nickname}的音乐库
+            </h1>
+
+            <div className="section-one">
+              <div className="liked-songs">
+                <div className="top">
+                  <span>span</span>
+                  <span>span</span>
+                  <span>span</span>
+                </div>
+                <div className="bottom">
+                  <div className="titles">
+                    <div className="title">我喜欢的音乐</div>
+                    <div className="sub-title">0 首歌</div>
+                  </div>
+                  <button>播放</button>
+                </div>
+              </div>
+              <div className="songs">
+                <div className="track-list">
+                  <div className="list">
+                    <img src={userDetail.profile.avatarUrl} alt="" />
+                    <div className="title-and-artist">
+                      <div className="container">
+                        <div className="title">吃突发</div>
+                        <div className="artist">吃土蕃 吃突发</div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="list">
+                    <img src={userDetail.profile.avatarUrl} alt="" />
+                    <div className="title-and-artist">
+                      <div className="container">
+                        <div className="title">吃突发</div>
+                        <div className="artist">吃土蕃 吃突发</div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="list">
+                    <img src={userDetail.profile.avatarUrl} alt="" />
+                    <div className="title-and-artist">
+                      <div className="container">
+                        <div className="title">吃突发</div>
+                        <div className="artist">吃土蕃 吃突发</div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="list">
+                    <img src={userDetail.profile.avatarUrl} alt="" />
+                    <div className="title-and-artist">
+                      <div className="container">
+                        <div className="title">吃突发</div>
+                        <div className="artist">吃土蕃 吃突发</div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="section-tow">section-tow</div>
           </div>
         ) : (
           <>
--- React/React_Basics/io-music-web-music/src/pages/mine/style.js
+++ React/React_Basics/io-music-web-music/src/pages/mine/style.js
@@ -4,11 +4,15 @@
  * @Author: by_mori
  * @Date: 2021-12-31 14:13:06
  * @LastEditors: by_mori
- * @LastEditTime: 2022-01-02 15:15:11
+ * @LastEditTime: 2022-01-02 19:36:13
  */
 import styled from 'styled-components';
 
 export const MineWrapper = styled.div`
+  --color-primary: #425de8;
+  --color-primary-bg: #ebeffd;
+  --color-seconder-bg: #f5f5f7;
+
   .content {
     background-color: #fff;
     min-height: 700px;
@@ -32,8 +36,130 @@ export const MineWrapper = styled.div`
     }
 
     .userDetail {
-      img {
-        width: 100%;
+      padding: 28px;
+      .userHeader {
+        font-size: 42px;
+        color: var(--color-text);
+        display: flex;
+        align-items: center;
+
+        .avatar {
+          height: 44px;
+          margin-right: 12px;
+          vertical-align: -7px;
+          border-radius: 50%;
+          border: rgba(0, 0, 0, 0.2);
+        }
+      }
+
+      .section-one {
+        display: flex;
+        margin-top: 24px;
+
+        .liked-songs {
+          flex: 3;
+          margin-top: 8px;
+          cursor: pointer;
+          border-radius: 16px;
+          padding: 18px 24px;
+          display: flex;
+          flex-direction: column;
+          transition: all 0.4s;
+          box-sizing: border-box;
+          background: var(--color-primary-bg);
+
+          .top {
+            flex: 1;
+            display: flex;
+            flex-wrap: wrap;
+            font-size: 14px;
+            opacity: 0.88;
+            color: var(--color-primary);
+          }
+
+          .bottom {
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            color: var(--color-primary);
+
+            .titles {
+              .title {
+                font-size: 24px;
+                font-weight: 700;
+              }
+
+              .sub-title {
+                font-size: 15px;
+                margin-top: 2px;
+              }
+            }
+
+            button {
+              margin-bottom: 2px;
+              display: flex;
+              justify-content: center;
+              align-items: center;
+              height: 44px;
+              width: 44px;
+              background: var(--color-primary);
+              border-radius: 50%;
+              transition: 0.2s;
+              box-shadow: 0 6px 12px -4px rgb(0 0 0 / 20%);
+              cursor: default;
+            }
+          }
+        }
+
+        .songs {
+          flex: 7;
+          margin-top: 8px;
+          margin-left: 36px;
+          overflow: hidden;
+
+          .track-list {
+            display: grid;
+            gap: 4px;
+            grid-template-columns: repeat(3, 1fr);
+            .list {
+              display: flex;
+              align-items: center;
+              padding: 8px;
+              border-radius: 12px;
+              -webkit-user-select: none;
+              -moz-user-select: none;
+              -ms-user-select: none;
+              user-select: none;
+
+              img {
+                height: 36px;
+                width: 36px;
+                border-radius: 6px;
+                margin-right: 14px;
+                cursor: pointer;
+              }
+              .title-and-artist {
+                flex: 1;
+                display: flex;
+                .container {
+                  display: flex;
+                  flex-direction: column;
+                  .title {
+                    font-size: 16px;
+                  }
+                  .artist {
+                    font-size: 12px;
+                  }
+                }
+              }
+
+              &:hover {
+                transition: all 0.3s;
+                background: var(--color-seconder-bg);
+              }
+            }
+          }
+        }
       }
     }
   }
