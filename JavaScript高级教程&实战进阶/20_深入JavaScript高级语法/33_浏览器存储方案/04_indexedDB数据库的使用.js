ef2dfb74d626438e292f160f814de576182ed35f
by_mori <by_mori@163.com>
2022-01-11 20:44:01 +0800

    indexedDB

--- JavaScript高级教程&实战进阶/20_深入JavaScript高级语法/33_浏览器存储方案/04_indexedDB数据库的使用.js
+++ JavaScript高级教程&实战进阶/20_深入JavaScript高级语法/33_浏览器存储方案/04_indexedDB数据库的使用.js
@@ -4,7 +4,7 @@
  * @Author: by_mori
  * @Date: 2022-01-11 19:02:26
  * @LastEditors: by_mori
- * @LastEditTime: 2022-01-11 19:30:14
+ * @LastEditTime: 2022-01-11 20:36:12
  */
 // 打开数据(和数据库建立连接)
 const dbRequest = indexedDB.open('momo');
@@ -62,12 +62,66 @@ for (let i = 0; i < btns.length; i++) {
         break;
       case 1:
         console.log('点击了 查询');
+
+        // 1.查询方式一(知道主键, 根据主键查询)
+        // const request = store.get(102);
+        // request.onsuccess = function (event) {
+        //   console.log(event.target.result);
+        // };
+
+        // 2.查询方式二:
+        const request = store.openCursor();
+        request.onsuccess = function (event) {
+          const cursor = event.target.result;
+          //   console.log(cursor);
+          if (cursor) {
+            // console.log(cursor.key, cursor.value);
+            // cursor.continue();
+
+            if (cursor.key === 101) {
+              console.log(cursor.key, cursor.value);
+            } else {
+              cursor.continue();
+            }
+          } else {
+            console.log('查询完成');
+          }
+        };
         break;
       case 2:
         console.log('点击了 删除');
+        const deleteRequest = store.openCursor();
+        deleteRequest.onsuccess = function (event) {
+          const cursor = event.target.result;
+          if (cursor) {
+            if (cursor.key === 101) {
+              cursor.delete();
+            } else {
+              cursor.continue();
+            }
+          } else {
+            console.log('删除完成');
+          }
+        };
         break;
       case 3:
         console.log('点击了 修改');
+        const updateRequest = store.openCursor();
+        updateRequest.onsuccess = function (event) {
+          const cursor = event.target.result;
+          if (cursor) {
+            if (cursor.key === 101) {
+              const value = cursor.value;
+              value.name = 'momo';
+              cursor.update(value);
+              console.log(cursor.key, cursor.value);
+            } else {
+              cursor.continue();
+            }
+          } else {
+            console.log('修改完成');
+          }
+        };
         break;
 
       default:
