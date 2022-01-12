59b393e6635449c676f51d61b4a0e83acb70c41b
by_mori <by_mori@163.com>
2022-01-12 17:25:10 +0800

    underscore使用

---
+++ JavaScript高级教程&实战进阶/20_深入JavaScript高级语法/36_防抖节流函数/01_debounce-v1-基本实现.js
@@ -0,0 +1,17 @@
+/*
+ * @Descripttion: io小栈 —— ioinn.cn
+ * @version: 0.0.1
+ * @Author: by_mori
+ * @Date: 2022-01-12 14:02:47
+ * @LastEditors: by_mori
+ * @LastEditTime: 2022-01-12 17:10:50
+ */
+// const divEl = document.querySelector('#cancel');
+
+// divEl.addEventListener('click', () => {
+//   console.log('div元素被点击');
+// });
+
+// setInterval(() => {
+//   document.querySelector('#cancel').click();
+// }, 1000);
