eefe602348995365a065cc0b03921ade0d472bc7
by_mori <by_mori@163.com>
2022-01-12 19:56:10 +0800

    debounce

--- JavaScript高级教程&实战进阶/20_深入JavaScript高级语法/36_防抖节流函数/02_debounce-v2-this-参数.js
+++ JavaScript高级教程&实战进阶/20_深入JavaScript高级语法/36_防抖节流函数/02_debounce-v2-this-参数.js
@@ -2,32 +2,22 @@
  * @Descripttion: io小栈 —— ioinn.cn
  * @version: 0.0.1
  * @Author: by_mori
- * @Date: 2022-01-12 14:02:47
+ * @Date: 2022-01-12 18:50:36
  * @LastEditors: by_mori
- * @LastEditTime: 2022-01-12 17:28:48
+ * @LastEditTime: 2022-01-12 18:50:36
  */
-// const divEl = document.querySelector('#cancel');
-
-// divEl.addEventListener('click', () => {
-//   console.log('div元素被点击');
-// });
-
-// setInterval(() => {
-//   document.querySelector('#cancel').click();
-// }, 1000);
-
 function debounce(fn, delay) {
   // 1.定义一个定时器, 保存上一次的定时器
   let timer = null;
 
   // 2.真正执行的函数
-  const _debounce = function () {
+  const _debounce = function (...args) {
     // 取消上一次的定时器
     if (timer) clearTimeout(timer);
     // 延迟执行
     timer = setTimeout(() => {
       // 外部传入的真正要执行的函数
-      fn();
+      fn.apply(this, args);
     }, delay);
   };
 
