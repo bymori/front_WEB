7f1cf114dece183adcee468f33f91bd2ed2b0c60
by_mori <by_mori@163.com>
2022-01-14 20:31:29 +0800

    Vite 按需加载

--- Vuejs/vue-threejs/src/App.vue
+++ Vuejs/vue-threejs/src/App.vue
@@ -1,3 +1,11 @@
+<!--
+ * @Descripttion: io小栈 —— ioinn.cn
+ * @version: 0.0.1
+ * @Author: by_mori
+ * @Date: 2022-01-14 19:30:16
+ * @LastEditors: by_mori
+ * @LastEditTime: 2022-01-14 20:26:51
+-->
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
@@ -7,6 +15,7 @@ import HelloWorld from './components/HelloWorld.vue'
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Hello Vue 3 + Vite" />
+  <a-button type="primary">Primary Button</a-button>
 </template>
 
 <style>
--- Vuejs/vue-threejs/vite.config.js
+++ Vuejs/vue-threejs/vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+/*
+ * @Descripttion: io小栈 —— ioinn.cn
+ * @version: 0.0.1
+ * @Author: by_mori
+ * @Date: 2022-01-14 19:30:16
+ * @LastEditors: by_mori
+ * @LastEditTime: 2022-01-14 20:25:58
+ */
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import Components from 'unplugin-vue-components/vite';
+import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-})
+  plugins: [vue(), Components({ resolvers: [AntDesignVueResolver()] })],
+});
