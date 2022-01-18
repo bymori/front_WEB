f6dfac6c1451635e527e75ee2feee24b71487627
by_mori <by_mori@163.com>
2022-01-18 19:41:01 +0800

    setTimeout

---
+++ 0.demo/setTimeout.js
@@ -0,0 +1,14 @@
+/*
+ * @Descripttion: io小栈 —— ioinn.cn
+ * @version: 0.0.1
+ * @Author: by_mori
+ * @Date: 2022-01-15 11:39:48
+ * @LastEditors: by_mori
+ * @LastEditTime: 2022-01-15 11:40:12
+ */
+setTimeout(() => {
+  console.log(1);
+}, 300);
+setTimeout(() => {
+  console.log(2);
+}, 222);
