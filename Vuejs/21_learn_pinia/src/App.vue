f2415ac11d098228a85920a11a9e8bcb4aafa0aa
by_mori <by_mori@163.com>
2022-01-21 17:10:36 +0800

    ShoppingCart

--- Vuejs/21_learn_pinia/src/App.vue
+++ Vuejs/21_learn_pinia/src/App.vue
@@ -4,9 +4,26 @@
  * @Author: by_mori
  * @Date: 2022-01-21 14:27:16
  * @LastEditors: by_mori
- * @LastEditTime: 2022-01-21 14:52:58
+ * @LastEditTime: 2022-01-21 17:08:52
 -->
+<template>
+  <h1>Pinia - 购物车示例</h1>
+
+  <hr />
+  <h2>商品列表</h2>
+  <ProductList />
+  <hr />
+  <ShoppingCart />
+</template>
+
 <script setup lang="ts">
+import ProductList from './components/ProductList.vue'
+import ShoppingCart from './components/ShoppingCart.vue'
+</script>
+
+<style></style>
+
+<!-- <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 import HelloWorld from './components/HelloWorld.vue'
@@ -25,4 +42,4 @@ import HelloWorld from './components/HelloWorld.vue'
   color: #2c3e50;
   margin-top: 60px;
 }
-</style>
+</style> -->
---
+++ Vuejs/21_learn_pinia/src/components/ProductList.vue
@@ -0,0 +1,29 @@
+<!--
+ * @Descripttion: io小栈 —— ioinn.cn
+ * @version: 0.0.1
+ * @Author: by_mori
+ * @Date: 2022-01-21 17:03:42
+ * @LastEditors: by_mori
+ * @LastEditTime: 2022-01-21 17:09:03
+-->
+<template>
+    <ul>
+        <li>
+            商品名称 - 商品价格
+            <br />
+            <button>添加到购物车</button>
+        </li>
+        <li>
+            商品名称 - 商品价格
+            <br />
+            <button>添加到购物车</button>
+        </li>
+        <li>
+            商品名称 - 商品价格
+            <br />
+            <button>添加到购物车</button>
+        </li>
+    </ul>
+</template>
+<script setup lang="ts">
+</script>
